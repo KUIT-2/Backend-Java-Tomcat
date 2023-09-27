e282b2a2975a7621b51e37609fd425ffa0936894
Mouon <ahemsapsldk@gmail.com>
2023-09-27 11:50:18 +0900

    parseRequestPath,serverFile 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,6 +2,7 @@ package webserver;
 
 import java.io.*;
 import java.net.Socket;
+import java.nio.file.Files;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,6 +14,8 @@ public class RequestHandler implements Runnable{
         this.connection = connection;
     }
 
+    /*RequestHandler 클래스의 run 메서드에서 클라이언트로부터의 요청을 받아들이고,
+    응답을 생성하여 클라이언트에게 반환*/
     @Override
     public void run() {
         log.log(Level.INFO, "New Client Connect! Connected IP : " + connection.getInetAddress() + ", Port : " + connection.getPort());
@@ -23,8 +26,32 @@ public class RequestHandler implements Runnable{
             byte[] body = "Hello World".getBytes();
             response200Header(dos, body.length);
             responseBody(dos, body);
+            //경로 할당
+            String requestPath = parseRequestPath(br);
+            if(requestPath.equals("/")||requestPath.equals("/index.html")){
+                serverFile("webapp/index.html",dos);
+            }else {
+
+            }
 
         } catch (IOException e) {
+            log.log(Level.SEVERE,e.getMessage());
+        }
+    }
+
+    /*Http 요청에서 경로 추출
+    * 첫번째 라인에서 두번째 구획에서 경로 추출*/
+    private String parseRequestPath(BufferedReader br) throws IOException{
+        String[] requestLine = br.readLine().split(" ");
+        return requestLine.length>1 ? requestLine[1] : "/";
+    }
+
+    private void serverFile(String filePath,DataOutputStream dos){
+        /*파일을 읽어오는 과정에서 IOException 발생 가능성 있어서*/
+        try{
+            File file = new File(filePath);
+            byte[] fileData = Files.readAllBytes(file.toPath());
+        }catch (IOException e){
             log.log(Level.SEVERE,e.getMessage());
         }
     }
