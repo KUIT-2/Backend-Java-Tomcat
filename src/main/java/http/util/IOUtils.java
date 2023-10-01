033bcf376047637504023e0e865467417f9ce6b2
Mouon <ahemsapsldk@gmail.com>
2023-10-01 17:49:19 +0900

    basic 참고해서 수정

--- src/main/java/http/util/IOUtils.java
+++ src/main/java/http/util/IOUtils.java
@@ -12,12 +12,6 @@ public class IOUtils {
      * @return
      * @throws IOException
      */
-    public static String readData(BufferedReader br, int contentLength) throws IOException {
-        char[] body = new char[contentLength];
-        br.read(body, 0, contentLength);
-        return String.copyValueOf(body);
-    }
-
     public static String readBody(BufferedReader br, int contentLength) throws IOException {
         char[] body = new char[contentLength];
         br.read(body, 0, contentLength);
