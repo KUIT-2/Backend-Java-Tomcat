996c1d5a5ee7a2fcabee8bfbdd59eb6026e8926c
Kjiw0n <rwd0904@naver.com>
2023-10-01 15:48:04 +0900

    Feat: 요구사항 1: index.html 반환하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -8,6 +8,7 @@ import java.util.logging.Logger;
 public class RequestHandler implements Runnable{
     Socket connection;
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
+    private static final String WEB_PORT = "webapp";
 
     public RequestHandler(Socket connection) {
         this.connection = connection;
@@ -20,12 +21,53 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String requestLine = br.readLine();
+            if (requestLine != null) {
+                String[] request = requestLine.split(" ");
+                if (request.length == 3 && request[0].equals("GET")) {
+
+                    String requestedPath = request[1];
+                    if (requestedPath.equals("/") || requestedPath.equals("/index.html")) {
+                        sendFile(dos, WEB_PORT + "/index.html");
+                    } else {
+                        send404NotFound(dos);
+                    }
+                }
+            }
+
+//            byte[] body = "Hello World".getBytes();
+//            response200Header(dos, body.length);
+//            responseBody(dos, body);
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
+        }
+    }
+
+    private void sendFile(DataOutputStream dos, String filePath) {
+        try {
+            File file = new File(filePath);
+            if (file.exists() && file.isFile()) {
+                byte[] fileBytes = readFileContents(file);
+                response200Header(dos, fileBytes.length);
+                responseBody(dos, fileBytes);
+            } else {
+                send404NotFound(dos);
+            }
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private byte[] readFileContents(File file) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            byte[] buffer = new byte[1024];
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            int bytesRead;
+            while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                baos.write(buffer, 0, bytesRead);
+            }
+            return baos.toByteArray();
         }
     }
 
@@ -35,6 +77,15 @@ public class RequestHandler implements Runnable{
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void send404NotFound(DataOutputStream dos) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found\r\n\r\n");
+            dos.flush();
         } catch (IOException e) {
             log.log(Level.SEVERE, e.getMessage());
         }
