a579094d12f3b1f0c72133dfd8e898c55c0a478f
encoreJeong <zzaibean@gmail.com>
2023-10-01 13:15:07 +0900

    Feat : RequestHandler 에서 GET / , GET /index.html 요청시 index.html 반환 하는기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -20,18 +20,47 @@ public class RequestHandler implements Runnable{
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
             DataOutputStream dos = new DataOutputStream(out);
 
-            byte[] body = "Hello World".getBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String line =  br.readLine();
+            if (line != null && (line.startsWith("GET / ") || line.startsWith("GET /index.html"))) {
+                byte[] body = loadFile("index.html");
+                if (body != null) {
+                    response200Header(dos, body.length);
+                    responseBody(dos, body);
+                } else {
+                    byte[] notFoundBody = "404 Not Found".getBytes();
+                    response404Header(dos, notFoundBody.length);
+                    responseBody(dos, notFoundBody);
+                }
+            }
 
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
     }
 
+    private byte[] loadFile(String filename) throws IOException {
+        File file = new File(filename);
+        if(file.exists()) {
+            return java.nio.file.Files.readAllBytes(file.toPath());
+        }
+        return null;
+    }
+
+
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
         try {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private void response404Header(DataOutputStream dos, int lengthOfBodyContent) {
+        try {
+            dos.writeBytes("HTTP/1.1 404 Not Found \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
             dos.writeBytes("\r\n");
