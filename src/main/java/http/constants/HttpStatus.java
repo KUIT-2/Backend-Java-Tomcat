9e4176b743adc2c50173c1487b4391003ebe3fad
woookk <woook27@naver.com>
2023-10-02 07:44:59 +0900

    Refactor: add HTTP constants: HttpMethod, HttpHeader, HttpStatus and RequestURL

---
+++ src/main/java/http/constants/HttpStatus.java
@@ -0,0 +1,17 @@
+package http.constants;
+
+public enum HttpStatus {
+    OK("OK"),
+    REDIRECT("Found");
+
+    private final String status;
+
+    HttpStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
+
