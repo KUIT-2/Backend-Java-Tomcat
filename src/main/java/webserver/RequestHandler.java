5e8b53179944d8377ab1749b441e3db7c9e54a96
DDongYul <comeme0101@naver.com>
2023-10-03 17:21:24 +0900

    Feat: 요구사항 2번 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,11 +1,16 @@
 package webserver;
 
+import db.MemoryUserRepository;
+import db.Repository;
 import http.util.IOUtils;
+import model.User;
 
 import java.io.*;
 import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,12 +19,16 @@ public class RequestHandler implements Runnable{
     private static final Logger log = Logger.getLogger(RequestHandler.class.getName());
     private static final String ROOT_URL = "./webapp";
     private static final String HOME_URL = "/index.html";
+    private static final String SIGNUP_URL = "/user/form.html";
     private static final String LOGIN_FAILED_URL = "/user/login_failed.html";
     private static final String LOGIN_URL = "/user/login.html";
     private static final String LIST_URL = "/user/list.html";
 
+    private final Repository repository;
+
     public RequestHandler(Socket connection) {
         this.connection = connection;
+        repository = MemoryUserRepository.getInstance();
     }
 
     @Override
@@ -35,12 +44,48 @@ public class RequestHandler implements Runnable{
             String requestUrl = startLines[1];
 
             byte[] body = new byte[0];
+            int requestContentLength = 10000;
+
+            while (true) {
+                final String line = br.readLine();
+                if (line.equals("")) {
+                    break;
+                }
+                // header info
+                if (line.startsWith("Content-Length")) {
+                    requestContentLength = Integer.parseInt(line.split(": ")[1]);
+                }
+            }
 
             /**
              * 요구사항 1번: index.html 반환하기
              */
             if (httpMethod.equals("GET") && (requestUrl.equals("/") || requestUrl.equals("/index.html"))) {
                 body = Files.readAllBytes(Paths.get(ROOT_URL+HOME_URL));
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+            }
+
+            /**
+             * 요구사항 2번: GET 방식으로 회원가입 하기
+             */
+            if (httpMethod.equals("GET") && (requestUrl.equals("/user/form.html"))) {
+                body = Files.readAllBytes(Paths.get(ROOT_URL+SIGNUP_URL));
+            }
+
+            String[] queryString = requestUrl.split("\\?");
+            if (queryString[0].equals("/user/signup")) {
+                String[] queryParams = queryString[1].split("&");
+                Map<String, String> queryParameter = new HashMap<>();
+                for (String queryParam : queryParams) {
+                    log.info(queryParam);
+                    String[] querySplit = queryParam.split("=");
+                    queryParameter.put(querySplit[0],querySplit[1]);
+                }
+                User user = new User(queryParameter.get("userId"), queryParameter.get("password"), queryParameter.get("name"), queryParameter.get("email"));
+                repository.addUser(user);
+                response302Header(dos,HOME_URL);
+                responseBody(dos, body);
             }
 
             response200Header(dos, body.length);
@@ -49,6 +94,22 @@ public class RequestHandler implements Runnable{
         } catch (IOException e) {
             log.log(Level.SEVERE,e.getMessage());
         }
+
+    }
+
+    private void response302Header(DataOutputStream dos, String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Location: " + path + "\r\n");
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage());
+        }
+    }
+
+    private Map<String, String> getQueryParameter(String queryString) {
+        return null;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
